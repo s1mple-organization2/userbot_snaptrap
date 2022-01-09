28aa2d11a9c3bc0ef775115de3749a2e74ccff38
dmity <dshalayko@gmail.com>
2022-01-10 00:28:09 +0300

    init commit фунция создания группы и отправки в группу сообщения

---
+++ main.py
@@ -0,0 +1,43 @@
+from pyrogram import Client
+
+app = Client("straptrap")
+
+"""
+Функция ищет из 5 последних диалогов диалог с название заказа 
+и отправляет сообщение в чат по чат айди
+"""
+
+
+async def send_msg(order_id, order_message):
+    async for dialog in app.iter_dialogs(5):
+        if (dialog.chat.title == 'snaptrap.online Order #' + order_id):
+            await app.send_message(dialog.chat.id, order_message)
+
+
+""" 
+Получить сообщение от бота
+Если сообщение пришло от snaptrap_bot или от бота 
+то парсим сообщение и создаём группу
+затем вызываем метод send_msg что бы отправить в чат список с заказом 
+"""
+
+
+@app.on_message()
+async def hello(client, message):
+    global order_id, order_message
+    chat_id = message.chat.id
+    # id юзербот
+    userbot_id = 5031837131
+    if message.chat.username == 'snaptrap_bot' or message.chat.type == 'bot':
+        tmp = message.text.split(":")
+        order_id = tmp[0]
+        user_id = tmp[1]
+        manager_id = tmp[2]
+        list_product = tmp[3]
+        total_price = tmp[4]
+        order_message = 'Order #' + order_id + '\n' + list_product + "\nTotal price:" + total_price + '$'
+        await app.create_group('snaptrap.online Order #' + order_id, [userbot_id, chat_id, user_id])
+        await send_msg(order_id, order_message)
+
+
+app.run()
